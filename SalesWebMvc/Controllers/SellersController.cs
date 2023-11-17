a33d9c2e7e7bd544c33fea17f453049cc875b54d
Hanspeter <hansdietiker2012@gmail.com>
2023-11-17 17:38:53 -0300

    Code/v1 Delete Seller

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -14,12 +14,13 @@ namespace SalesWebMvc.Controllers
     {
         private readonly SellerService _sellerService;
         private readonly DepartmentService _departmentService;
+
         public SellersController(SellerService sellerService, DepartmentService departmentService)
         {
             _sellerService = sellerService;
             _departmentService = departmentService;
         }
-        
+
         public IActionResult Index()
         {
             var list = _sellerService.FindAll();
@@ -29,7 +30,7 @@ namespace SalesWebMvc.Controllers
         public IActionResult Create()
         {
             var departments = _departmentService.FindAll();
-            var viewModel = new SellerFormViewModel{Departments = departments};
+            var viewModel = new SellerFormViewModel { Departments = departments };
             return View(viewModel);
         }
 
@@ -38,6 +39,30 @@ namespace SalesWebMvc.Controllers
         public IActionResult Create(SellerModel seller)
         {
             _sellerService.Insert(seller);
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var obj = _sellerService.FindById(id.Value);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+            
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(int id)
+        {
+            _sellerService.Remove(id);
             return RedirectToAction(nameof(Index));
         }
     }
--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -21,4 +21,17 @@ public class SellerService
         _context.Add(seller);
         _context.SaveChanges();
     }
+
+    public SellerModel FindById(int id)
+    {
+        return _context.Seller.FirstOrDefault(obj => obj.Id == id);
+    }
+
+    public void Remove(int id)
+    {
+        var obj = _context.Seller.Find(id);
+        _context.Seller.Remove(obj);
+        _context.SaveChanges();
+    }
+    
 }
