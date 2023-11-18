5a12ce477cad0ad322e49a1ae83e3c0d558adffb
Hanspeter <hansdietiker2012@gmail.com>
2023-11-18 17:39:18 -0300

    Code/v1 Custom Page Error

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -1,9 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+
+using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.CodeAnalysis.Elfie.Serialization;
 using SalesWebMvc.Models;
 using SalesWebMvc.Models.ViewModels;
 using SalesWebMvc.Services;
@@ -71,20 +68,35 @@ namespace SalesWebMvc.Controllers
         {
             if (id == null)
             {
-                return NotFound();
+                return RedirectToAction(nameof(Error), new { message = "Id não fornecido" });
             }
 
             var obj = _sellerService.FindById(id.Value);
             if (obj == null)
             {
-                return NotFound();
+                 return RedirectToAction(nameof(Error), new { message = "Id não encontrada" });
             }
 
             List<DepartmentModel> departments = _departmentService.FindAll();
             SellerFormViewModel viewModel = new SellerFormViewModel { Seller = obj, Departments = departments };
             return View(viewModel);
-        } 
-        
+        }
+
+        public IActionResult Details(int? id)
+        {
+            if (id==null)
+            {
+                 return RedirectToAction(nameof(Error), new { message = "Id não fornecida" });
+            }
+
+            var obj = _sellerService.FindById(id.Value);
+            if (obj==null)
+            {
+                return RedirectToAction(nameof(Error), new { message = "Id não encontrada" });
+            }
+
+            return View();
+        }
         
         [HttpPost]
         [ValidateAntiForgeryToken]
@@ -92,21 +104,31 @@ namespace SalesWebMvc.Controllers
         {
             if (id != seller.Id)
             {
-                return BadRequest();
+                return RedirectToAction(nameof(Error), new { message = "Id não corresponde" });
             }
             try
             {
                 _sellerService.Update(seller);
                 return RedirectToAction(nameof(Index));
             }
-            catch (NotFoundException)
+            catch (NotFoundException e)
             {
-                return NotFound();
+                 return RedirectToAction(nameof(Error), new { message = e.Message });
             }
-            catch (DbConcurrencyException)
+            catch (DbConcurrencyException e)
             {
-                return BadRequest();
+                return RedirectToAction(nameof(Error), new { message = e.Message });
             }
+
         }
+            public IActionResult Error(string message)
+            {
+                var viewModel = new ErrorViewModel
+                {
+                    Message = message,
+                    RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier
+                };
+                return View(viewModel);
+            }
     }
 }
